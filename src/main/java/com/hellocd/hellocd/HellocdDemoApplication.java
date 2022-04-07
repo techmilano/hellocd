423d9e892ebb5095cf93f1af73127e8d6a751dd1
techmilano <mhniceguy@gmail.com>
2022-04-07 18:19:47 -0400

    Initial spring version

---
+++ src/main/java/com/hellocd/hellocd/HellocdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hellocd.hellocd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellocdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellocdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hellocd/hellocd/HellocdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellocd.hellocd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellocdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
